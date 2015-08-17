be8c9b6fc8e297f2c07d758a2368859874aea7b0
Dan Tong <tongvtdan@outlook.com>
2015-08-17 12:07:25 +0700

    Calculate values

--- tipCalculator/tipCalculator/ViewController.swift
+++ tipCalculator/tipCalculator/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var buildAmountTextField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    var tipPercent:Double = 0.1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTextChanging(sender: UITextField) {
+        if sender.text != "" {
+            let billAmount: Double = Double(sender.text.toInt()!)
+            let tipAmount: Double = billAmount * tipPercent
+            let totalAmount : Double = billAmount + tipAmount
+            println("Amount: \(billAmount) , \(tipAmount) , \(totalAmount)")
+            tipLabel.text = String(format: "%0.1f",tipAmount)
+            totalAmountLabel.text = String(format: "%0.1f",totalAmount)
+        }
+    }
+    
 
 }
 
