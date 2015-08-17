9304a84800ebe56d15a48f8d118093977376559a
Dan Tong <tongvtdan@outlook.com>
2015-08-17 14:59:39 +0700

    Add slider to change tip percent

--- tipCalculator/tipCalculator/ViewController.swift
+++ tipCalculator/tipCalculator/ViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var buildAmountTextField: UITextField!
+    @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    
     var tipPercent:Double = 0.1
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,12 +32,32 @@ class ViewController: UIViewController {
             let billAmount: Double = Double(sender.text.toInt()!)
             let tipAmount: Double = billAmount * tipPercent
             let totalAmount : Double = billAmount + tipAmount
-            println("Amount: \(billAmount) , \(tipAmount) , \(totalAmount)")
-            tipLabel.text = String(format: "%0.1f",tipAmount)
-            totalAmountLabel.text = String(format: "%0.1f",totalAmount)
+            tipLabel.text = String(format: "$%0.1f",tipAmount)
+            totalAmountLabel.text = String(format: "$%0.1f",totalAmount)
         }
     }
     
+    func updateAmount(){
+        if billAmountTextField.text != "" {
+            let billAmount: Double = Double(billAmountTextField.text.toInt()!)
+            let tipAmount: Double = billAmount * tipPercent
+            let totalAmount : Double = billAmount + tipAmount
+            //            println("Amount: \(billAmount) , \(tipAmount) , \(totalAmount)")
+            tipLabel.text = String(format: "$%0.1f",tipAmount)
+            totalAmountLabel.text = String(format: "$%0.1f",totalAmount)
+        }
 
+    }
+    @IBAction func tipPercentSliderOnChanged(sender: UISlider) {
+        tipPercentLabel.text = String(format: "%0.f%%",sender.value)
+        tipPercent = Double(sender.value)/100
+        updateAmount()
+        
+    }
+    
+    // MARK: Keyboard hide on Tap
+    @IBAction func onTapScreen(sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
 }
 
